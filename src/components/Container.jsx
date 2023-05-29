88879c2f2dd96efc2f32e6bf93e2719eef0b0e85
Ankur Auti <ankurauti@gmail.com>
2023-05-29 19:22:22 +0000

    updated

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -8,12 +8,23 @@ function Container() {
   const [numWins, setNumWins] = useState(0);
   const [numLosses, setNumLosses] = useState(0);
   const [numTies, setNumTies] = useState(0);
+  const [final,setFinal] = useState('')
+  const [totalClicks, setTotalClicks] = useState(0);
+  const [showContent, setShowContent] = useState(false);
+    
+    
+    
 
   const handleButtonClick = (id) => {
     setSelectedButtonId(id);
     playGame(id);
+    setTotalClicks(totalClicks + 1);
+    
   };
 
+  const isDisabled = totalClicks >= 10;
+  const remainingClicks = 10 - totalClicks;
+ 
   const playGame = (selectedId) => {
     const array = ["rock", "paper", "scissor"];
     const choice = array[Math.floor(Math.random() * array.length)];
@@ -21,32 +32,56 @@ function Container() {
     setPcChoice(choice);
 
     if (choice === selectedId) {
-      setWin("It's a tie");
+      setWin("tie");
       setNumTies((prevNumTies) => prevNumTies + 1);
     } else if (
       (choice === "rock" && selectedId === "scissor") ||
       (choice === "paper" && selectedId === "rock") ||
       (choice === "scissor" && selectedId === "paper")
     ) {
-      setWin("You lose");
+      setWin("lose");
       setNumLosses((prevNumLosses) => prevNumLosses + 1);
     } else {
-      setWin("You win");
+      setWin("win");
       setNumWins((prevNumWins) => prevNumWins + 1);
     }
   };
 
+
+  const handleCompareClick = () => {
+    if (numWins > numLosses) {
+      setFinal("🥳 You won!");
+    } else if (numWins < numLosses) {
+      setFinal("😟 You lost!");
+    } else {
+      setFinal("😑 It's a tie!");
+    }
+    
+    
+  
+    setSelectedButtonId(null);
+    setWin("");
+    setPcChoice("");
+    setNumWins(0);
+    setNumLosses(0);
+    setNumTies(0);
+    setTotalClicks(0)
+    setShowContent(true)
+    setTimeout(() => setShowContent(false), 3000);
+  };
   return (
     <div className="container">
-      <div className="stats">
+      {/* <div className="stats">
         <p>Wins: {numWins}</p>
         <p>Losses: {numLosses}</p>
         <p>Ties: {numTies}</p>
-      </div>
+      </div> */}
+      <h3 className="count"> {remainingClicks?`tries: ${remainingClicks}`:"please submit to see result"}</h3>
       <div className="icons">
         <button
           className={`icon rock ${selectedButtonId === "rock" ? "active" : ""}`}
           onClick={() => handleButtonClick("rock")}
+          disabled={isDisabled}
         >
           🪨
         </button>
@@ -55,6 +90,7 @@ function Container() {
             selectedButtonId === "paper" ? "active" : ""
           }`}
           onClick={() => handleButtonClick("paper")}
+          disabled={isDisabled}
         >
           📄
         </button>
@@ -63,13 +99,18 @@ function Container() {
             selectedButtonId === "scissor" ? "active" : ""
           }`}
           onClick={() => handleButtonClick("scissor")}
+          disabled={isDisabled}
         >
           ✂️
         </button>
       </div>
-      <p>You choose: {selectedButtonId}</p>
-      <p>PC choice: {pcChoice}</p>
-      <h2>{win}</h2>
+      <p>👤: {selectedButtonId}</p>
+      <p>🤖: {pcChoice}</p>
+      <h3>{win}</h3>
+      <button className="submit" onClick={handleCompareClick}>
+  {showContent ? <h2>{final}</h2> : totalClicks < 10 ? "Submit":"Submit"}
+</button>
+
     </div>
   );
 }
